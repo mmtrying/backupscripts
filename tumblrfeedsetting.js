2553375078e32ea425d32c17ede971c787064ed6
mmtrying <mmtrying@gmail.com>
2016-08-15 18:33:16 +0800

    Delete tumblrfeedsetting.js

--- tumblrfeedsetting.js
+++
@@ -1,9 +0,0 @@
-var tumblrFeedSetting = {
-  list_item_class_name: '',
-  append_to_ul_element: null,
-  date_format: false,  //string:: default as '<span class="fp-dateframe"><span class="fp-subject">Date</span>: <span class="fp-date">[_day2_]-[_month_]-[_year_]</span> (<span class="fp-weekday">[_weekday_]</span>)</span>'
-  time_format: false,  //string:: default as '<span class="fp-timeframe"><span class="fp-subject">Time</span>: <span class="fp-time">[_hour_]:[_minute_]:[_second_]</span></span>'
-  expression_month: null,  //array for 12 strings:: default as ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
-  expression_weekday: null,  //array for 7 strings:: default as ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
-  tagging_staticpage: ''  //string:: default normal tagging as no string, but can assign staticpage tagged by parameter, eg: '/label'.
-}; runTumblrJson(tumblr_api_read,tumblrFeedSetting);
