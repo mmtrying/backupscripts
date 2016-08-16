f8584aa8995d1bd2250a535e5b7d32c7634e54c1
mmtrying <mmtrying@gmail.com>
2016-08-16 13:41:20 +0800

    Update av2u_tumblrjson.js

--- av2u_tumblrjson.js
+++ av2u_tumblrjson.js
@@ -179,7 +179,6 @@ function runTumblrJson($json,$setting){
             h += '</div>';
 			
             if(h){
-            	d.getElementById('testingtext').value +=h;
                 if(aElmUL){
                     wLi=d.createElement('li');
                     wLi.className = cnLi;
