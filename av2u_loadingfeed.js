39e39c3b4b0113101e8d1aa94ec44defc425a4e6
mmtrying <mmtrying@gmail.com>
2016-08-17 17:58:33 +0800

    Update av2u_loadingfeed.js

--- av2u_loadingfeed.js
+++ av2u_loadingfeed.js
@@ -8,10 +8,13 @@ var $totalpost = $json?parseInt($json['posts-total']||0):0;
 (function(){
   var dc = document, iThis = parseInt(getparameter('page')||1)||1;
   var n = parseInt(numberFeedPerPage||0)||0, sT = Math.ceil(n/50);
-  for(var i=1;i<=sT;i++){
-    dc.write('<script type="text/javascript" src="/api/read/json?start='+(((iThis-1)*$totalpost)+(50*(sT-1))+1)+'&num='+(n?(n-((i-1)*50)):50)+'"></script>');
-    dc.write('<script type="text/javascript">runTumblrJson(tumblr_api_read,av2u_feedSetting);</script>');
+  var r = $totalpost-(n*(iThis-1));
+  if(r>0){
+    for(var i=1;i<=sT;i++){
+      dc.write('<script type="text/javascript" src="/api/read/json?start='+(((iThis-1)*$totalpost)+(50*(sT-1))+1)+'&num='+((r>sT*50)?50:(r-(sT-(1*50))))+'"></script>');
+      dc.write('<script type="text/javascript">runTumblrJson(tumblr_api_read,av2u_feedSetting);</script>');
+    }
+  } else {
+    dc.write('<div>Sorry! No more page is existed. Please report to us for any problems.</div>');
   }
 })();
-
-
