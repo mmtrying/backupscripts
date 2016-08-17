09df3d208d1c574b29a833c663ce971ee6000047
mmtrying <mmtrying@gmail.com>
2016-08-17 12:34:06 +0800

    Update av2u_loadingfeed.js

--- av2u_loadingfeed.js
+++ av2u_loadingfeed.js
@@ -1,4 +1,35 @@
 var numberFeedPerPage = 80;
+
+//add '<script type="text/javascript" src="/api/read/json?start=1&num=0"></script>' before this script
+var $json = tumblr_api_read;
+
+//unknown item
+var $feeds = $json?(typeof $json.tumblelog=='undefined'?null:($json.tumblelog.feeds||null)):null;
+if($feeds){
+  if($feeds.length>0){
+    var $tempfeeds = '';
+    for(var $rIdx=0;$rIdx<$feeds.length;$rIdx++){
+      if($rIdx>0) $tempfeeds += ', ';
+      $tempfeeds += $feeds[$rIdx]||'';
+    }; $feeds = $tempfeeds; $tempfeeds = null;
+  } else {
+    $feeds='';
+  }
+}
+
+//Site Info
+var $title = $json?(typeof $json.tumblelog=='undefined'?null:($json.tumblelog.title||null)):null;
+var $description = $json?(typeof $json.tumblelog=='undefined'?null:($json.tumblelog.description||null)):null;
+var $name = $json?(typeof $json.tumblelog=='undefined'?null:($json.tumblelog.name||null)):null;
+var $timezone = $json?(typeof $json.tumblelog=='undefined'?null:($json.tumblelog.timezone||null)):null;
+var $domain = $json?(typeof $json.tumblelog=='undefined'?null:($json.tumblelog.cname||null)):null;
+
+//Post Info
+var $indexbegin = $json?parseInt($json['posts-start']||0):null;
+var $totalpost = $json?parseInt($json['posts-total']||0):null;
+var $blogtype = $json?($json['posts-type']||false):null;
+
+
 (function(){
   var dc = document, iThis = parseInt(getparameter('page')||0)||0;
   var n = parseInt(numberFeedPerPage||0)||0, sT = Math.ceil(n/50);
