1aa8692774f668e88c411b86a0eb5dac6b02bb40
mmtrying <mmtrying@gmail.com>
2016-08-17 10:25:41 +0800

    Create av2u_loadingfeed.js

---
+++ av2u_loadingfeed.js
@@ -0,0 +1,9 @@
+(function(){
+  var iThis = parseInt(getparameter('page')||0)||0;
+  var sT = Math.ceil((parseInt(numberFeedPerPage||0)||0)/50);
+  var n = parseInt(numberFeedPerPage||0)||0;
+  for(var i=1;i<=sT;i++){
+    document.write('<script type="text/javascript" src="/api/read/json?start='+(iThis?((iThis*50)-49):1)+'&num='+(n?(n-((i-1)*50)):50)+'"></script>');
+    runTumblrJson(tumblr_api_read,av2u_feedSetting);
+  }
+})();
