67ecc6189b37fb2584250c266927eae6800fc7e5
mmtrying <mmtrying@gmail.com>
2016-08-17 11:32:53 +0800

    Update av2u_loadingfeed.js

--- av2u_loadingfeed.js
+++ av2u_loadingfeed.js
@@ -1,10 +1,9 @@
 var numberFeedPerPage = 80;
 (function(){
-  var iThis = parseInt(getparameter('page')||0)||0;
-  var sT = Math.ceil((parseInt(numberFeedPerPage||0)||0)/50);
-  var n = parseInt(numberFeedPerPage||0)||0;
+  var dc = document, iThis = parseInt(getparameter('page')||0)||0;
+  var n = parseInt(numberFeedPerPage||0)||0, sT = Math.ceil(n/50);
   for(var i=1;i<=sT;i++){
-    document.write('<script type="text/javascript" src="/api/read/json?start='+(iThis?((iThis*50)-49):1)+'&num='+(n?(n-((i-1)*50)):50)+'"></script>');
-    runTumblrJson(tumblr_api_read,av2u_feedSetting);
+    dc.write('<script type="text/javascript" src="/api/read/json?start='+(iThis?((iThis*50)-49):1)+'&num='+(n?(n-((i-1)*50)):50)+'"></script>');
+    dc.write('<script type="text/javascript">runTumblrJson(tumblr_api_read,av2u_feedSetting);</script>');
   }
 })();
