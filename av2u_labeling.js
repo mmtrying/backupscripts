fbb65369c5c6b357128d033117f085745dfce2ca
mmtrying <mmtrying@gmail.com>
2016-08-18 09:08:44 +0800

    Create av2u_labeling.js

---
+++ av2u_labeling.js
@@ -0,0 +1,34 @@
+var numberFeedPerPage = 80;
+
+
+var thisPageLabel = '';
+(function(){
+  var d = document, u = decodeURIComponent(d.URL);
+  var a = (location||window.location).pathname;
+  if(a.indexOf('/label')==0){
+    if(u.indexOf('/label?=')!=-1){
+      thisPageLabel = u.split('/label?=')[1];
+      if(u.indexOf('&')!=-1) thisPageLabel = thisPageLabel.split('&')[0];
+    }
+  }
+})();
+
+
+//add '<script type="text/javascript" src="/api/read/json?start=1&num=0"></script>' before this script
+var $json = tumblr_api_read;
+var $totalpost = $json?parseInt($json['posts-total']||0):0;
+
+(function(){
+  var dc = document, iThis = parseInt(getparameter('page')||1)||1;
+  var n = parseInt(numberFeedPerPage||0)||0
+  var r = $totalpost-(n*(iThis-1)), sT = Math.ceil(r/50);
+  if(r>0){
+    for(var i=1;i<=sT;i++){
+      dc.write('<script type="text/javascript" src="/api/read/json?start='+(((iThis-1)*$totalpost)+((i-1)*50)+1)+'&num='+((r>i*50)?50:(r-((i-1)*50)))+(thisPageLabel?('&tagged='+thisPageLabel):'')+'"></script>');
+      dc.write('<script type="text/javascript">runTumblrJson(tumblr_api_read,av2u_feedSetting);</script>');
+    }
+  } else {
+    dc.write('<div>Sorry! No more page is existed. Please report to us for any problems.</div>');
+  }
+})();
+
