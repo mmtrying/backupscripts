d4d037c41a59d0596d0346c792ba84fd43312754
mmtrying <mmtrying@gmail.com>
2016-08-18 19:02:12 +0800

    Update av2u_loadingfeed.js

--- av2u_loadingfeed.js
+++ av2u_loadingfeed.js
@@ -1,6 +1,6 @@
 var numberFeedPerPage = 80;
 
-//add '<script type="text/javascript" src="/api/read/json?start=1&num=0"></script>' before this script
+//add '<script type="text/javascript" src="/api/read/json?start=0&num=0"></script>' before this script
 var $json = tumblr_api_read;
 var $totalpost = $json?parseInt($json['posts-total']||0):0;
 
@@ -11,7 +11,7 @@ var $totalpost = $json?parseInt($json['posts-total']||0):0;
   var r = $totalpost-(n*(iThis-1)), sT = Math.ceil(r/50);
   if(r>0){
     for(var i=1;i<=sT;i++){
-      dc.write('<script type="text/javascript" src="/api/read/json?start='+(((iThis-1)*$totalpost)+((i-1)*50)+1)+'&num='+((r>i*50)?50:(r-((i-1)*50)))+'"></script>');
+      dc.write('<script type="text/javascript" src="/api/read/json?start='+(((iThis-1)*$totalpost)+((i-1)*50))+'&num='+((r>i*50)?50:(r-((i-1)*50)))+'"></script>');
       dc.write('<script type="text/javascript">runTumblrJson(tumblr_api_read,av2u_feedSetting);</script>');
     }
   } else {
